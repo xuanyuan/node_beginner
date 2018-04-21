7ed7f9bf3cdd6285e168c1caebd36d09163691e4
zhangdj <zhangdj@eastsoft.com.cn>
2018-04-21 17:05:38 +0800

    feat(使用外部模块):

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var http = require('http');
 
-function onRequest(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello world");
-    response.end();
-};
+function start () {
+    function onRequest(request, response) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello world");
+        response.end();
+    };
 
-// http.createServer
-var server = http.createServer(onRequest);
+    // http.createServer
+    var server = http.createServer(onRequest);
 
-server.listen(3000);
-console.log(server);
+    server.listen(3000);
+    console.log('Server has started');
+}
+
+// 此处可以不一致，使用前者
+exports.run = start;
